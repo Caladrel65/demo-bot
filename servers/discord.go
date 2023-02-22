a0e83ff9f668a2f13bd05e72311420d789c6f3f3
Joshua Francis <jnf333@gmail.com>
2023-02-21 20:36:07 -0600

    Moved discordToken into main function temporarily

--- servers/discord.go
+++ servers/discord.go
@@ -5,13 +5,13 @@ import (
 	log "github.com/sirupsen/logrus"
 )
 
-var discordToken = "Token Goes Here" // Not actually. Your token should exist in the environment, or better yet, in some form of secrets. You should not store your token anywhere GitHub can access it! Include that location in your .gitignore!
-
 // We want to use the following line in the powershell terminal:
 // go build tutorial.go; ./tutorial.exe
 // Don't forget to use .exe not .go for the executable, silly!
 
 func main() {
+	discordToken := "Token Goes Here" // Not actually. Your token should exist in the environment, or better yet, in some form of secrets. You should not store your token anywhere GitHub can access it! Include that location in your .gitignore!
+
 	log.Trace("discord handler booting up")
 	// TODO: Create Discord server-API object
 
